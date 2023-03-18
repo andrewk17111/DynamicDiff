8fec8de7daeca6053fb8660f797028cc0745cff5
andrewk17111 <andrewk17111@gmail.com>
2023-03-18 17:19:57 -0400

    Implement Shallow File Comparison
    Implemented shallow file comparison

--- DynamicDiff/FileData.cs
+++ DynamicDiff/FileData.cs
@@ -6,13 +6,27 @@ internal struct FileData
     public string Location { get; }
     public string Path
         => System.IO.Path.Combine(Location, Name);
-    public ulong Size { get; }
+    public long Size { get; }
     public DateTime Created { get; }
     public DateTime Modified { get; }
     public bool IsHidden { get; }
     public bool IsReadOnly { get; }
 
-    public FileData(string name, string location, ulong size, DateTime created, DateTime modified, bool is_hidden, bool is_read_only)
+    public FileData(string path)
+    {
+        FileInfo file = new FileInfo(path);
+
+        Name = file.Name;
+        Location = file.DirectoryName;
+        Size = file.Length;
+        Created = file.CreationTime;
+        Modified = file.LastAccessTime;
+        IsHidden = file.Attributes.HasFlag(FileAttributes.Hidden);
+        IsReadOnly = file.Attributes.HasFlag(FileAttributes.ReadOnly);
+    }
+
+    private FileData(string name, string location, long size, DateTime created, DateTime modified, bool is_hidden,
+        bool is_read_only)
     {
         Name = name;
         Location = location;
@@ -25,4 +39,7 @@ internal struct FileData
 
     public string[] Read()
         => throw new NotImplementedException();
+
+    public override string ToString()
+        => $"{Size} {Created} {Modified} {(IsHidden ? "H" : "")}{(IsReadOnly ? "R" : "")} {Name}";
 }
--- DynamicDiff/Program.cs
+++ DynamicDiff/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DynamicDiff;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage: DynamicDiff.exe <source_file> <target_file>");
+    return;
+}
+
+FileData source = new FileData(args[0]);
+FileData target = new FileData(args[1]);
+
+if (source.Size != target.Size)
+    Console.WriteLine("\u001b[41mFiles are different sizes.");
+else if (source.Created != target.Created)
+    Console.WriteLine("Files were created at different times.");
+else if (source.Modified != target.Modified)
+    Console.WriteLine("Files were modified at different times.");
+else if (source.IsHidden != target.IsHidden)
+    Console.WriteLine("Files have different hidden attributes.");
+else if (source.IsReadOnly != target.IsReadOnly)
+    Console.WriteLine("Files have different read-only attributes.");
+else
+    Console.WriteLine("Files appear the same");
