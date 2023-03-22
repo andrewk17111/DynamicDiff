07c111d2f9e2b81dc1bc05d0b976ea00f542ebb6
andrewk17111 <andrewk17111@gmail.com>
2023-03-22 14:13:14 -0400

    Change DirectoryData Children
    Changed how the children of DIrectoryData is stored

--- DynamicDiff/DirectoryData.cs
+++ DynamicDiff/DirectoryData.cs
@@ -2,17 +2,22 @@
 
 internal struct DirectoryData
 {
+    private readonly FileInfo[] _files;
+    private readonly DirectoryInfo[] _directories;
+
     public readonly string Name;
     public readonly string Location;
     public readonly DateTime Created;
     public readonly DateTime Modified;
     public readonly bool IsHidden;
     public readonly bool IsReadOnly;
-    public readonly string[] Files;
-    public readonly string[] Directories;
 
     public readonly string Path
         => System.IO.Path.Combine(Location, Name);
+    public readonly string[] Files
+        => _files.Select(f => f.Name).ToArray();
+    public readonly string[] Directories
+        => _directories.Select(d => d.Name).ToArray();
     public readonly uint FileCount
         => (uint)Files.Length;
     public readonly uint DirectoryCount
@@ -29,8 +34,9 @@ internal struct DirectoryData
         Modified = dir.LastAccessTime;
         IsHidden = dir.Attributes.HasFlag(FileAttributes.Hidden);
         IsReadOnly = dir.Attributes.HasFlag(FileAttributes.ReadOnly);
-        Files = dir.GetFiles().Select(f => f.FullName).ToArray();
-        Directories = dir.GetDirectories().Select(d => d.FullName).ToArray();
+
+        _files = dir.GetFiles();
+        _directories = dir.GetDirectories();
     }
 
     public override string ToString()
